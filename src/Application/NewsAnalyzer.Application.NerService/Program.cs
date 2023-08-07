4c4eebbf1e68209c5d0158f1bb02227d5802d277
Денис <@DESKTOP-EKTHKC1>
2023-08-07 17:00:27 +0300

    remove comments and exta code

--- src/Application/NewsAnalyzer.Application.NerService/Program.cs
+++ src/Application/NewsAnalyzer.Application.NerService/Program.cs
@@ -1,4 +1,4 @@
-using NewsAnalyzer.Application.NerService;
+using NewsAnalyzer.Application.NerService.Extensions;
 using NewsAnalyzer.Application.NerService.Services;
 using NewsAnalyzer.Core.Abstractions;
 using NewsAnalyzer.Core.Events;
@@ -9,21 +9,15 @@ using NewsAnalyzer.Infrastructure.RabbitMqService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
 builder.Services.AddGrpc();
 builder.Services.AddHostedService<BackgroundNerService>();
 builder.Services.AddSingleton<INerService, CatalystNerService>();
 builder.Services.AddSingleton<IMessengerConsumerService<NewsLoadedEventArgs>, RabbitMqMessengerConsumerService<NewsLoadedEventArgs>>();
 builder.Services.AddSingleton<INamedEntityFormRepository, NamedEntityFormEfCoreRepository>();
-builder.Services.AddServicesConfiguration();
+builder.Services.AddServicesConfiguration(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
--- src/Application/NewsAnalyzer.Application.NerService/Services/BackgroundNerService.cs
+++ src/Application/NewsAnalyzer.Application.NerService/Services/BackgroundNerService.cs
@@ -1,105 +1,71 @@
-﻿using Grpc.Net.Client;
-using NewsAnalyzer.Application.NewsService;
+﻿using NewsAnalyzer.Application.NewsService;
 using NewsAnalyzer.Core.Abstractions;
 using NewsAnalyzer.Core.Events;
 using NewsAnalyzer.Core.Models;
 using NewsAnalyzer.Infrastructure.RabbitMqService.Abstractions;
+using static NewsAnalyzer.Application.NewsService.ApplicationNews;
 
-namespace NewsAnalyzer.Application.NerService.Services
+namespace NewsAnalyzer.Application.NerService.Services;
+
+public class BackgroundNerService : BackgroundService
 {
-    public class BackgroundNerService : BackgroundService
+    private readonly INerService _nerService;
+    private readonly IMessengerConsumerService<NewsLoadedEventArgs> _messengerConsumerService;
+    private readonly INamedEntityFormRepository _namedEntityFormRepository;
+    private readonly ApplicationNews.ApplicationNewsClient _applicationNewsClient;
+
+    public BackgroundNerService(
+        INerService nerService, 
+        IMessengerConsumerService<NewsLoadedEventArgs> messengerConsumerService, 
+        INamedEntityFormRepository namedEntityFormAsyncRepository, 
+        ApplicationNewsClient applicationNewsClient)
     {
-        private readonly INerService _nerService;
-        private readonly IMessengerConsumerService<NewsLoadedEventArgs> _messengerConsumerService;
-        private readonly INamedEntityFormRepository _namedEntityFormRepository;
-        private readonly ApplicationNews.ApplicationNewsClient _applicationNewsClient;
+        _nerService = nerService;
+        _messengerConsumerService = messengerConsumerService;
+        _namedEntityFormRepository = namedEntityFormAsyncRepository;
+        _applicationNewsClient = applicationNewsClient;
+    }
 
-        public BackgroundNerService(INerService nerService, IMessengerConsumerService<NewsLoadedEventArgs> messengerConsumerService, INamedEntityFormRepository namedEntityFormAsyncRepository)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _messengerConsumerService.Received += _messengerConsumerService_Received;
+    }
+
+    private void _messengerConsumerService_Received(object? sender, Infrastructure.RabbitMqService.Events.MessageReceivedEventArgs<NewsLoadedEventArgs> e)
+    {
+        var newsResponse = _applicationNewsClient.GetNews(new NewsRequest { Id = e.Message.NewsId.ToString() });
+        
+        var news = new News(Guid.Parse(newsResponse.Id),
+                            newsResponse.SourceName,
+                            newsResponse.Title,
+                            newsResponse.Text,
+                            newsResponse.PublishDate.ToDateTime());
+        var namedEntityForms = _nerService.GetNamedEntityFormsFromNews(news);
+        
+        var exsistedNamedEntityForms = _namedEntityFormRepository.GetWhere(entity => namedEntityForms
+                                                                                         .Select(e => e.Value)
+                                                                                         .Contains(entity.Value));
+
+        var unexsistedNamedEntityForms = new List<NamedEntityForm>();
+        foreach (var namedEntityForm in namedEntityForms)
         {
-            _nerService = nerService;
-            _messengerConsumerService = messengerConsumerService;
-            _namedEntityFormRepository = namedEntityFormAsyncRepository;
-            _applicationNewsClient = new ApplicationNews.ApplicationNewsClient(GrpcChannel.ForAddress("https://localhost:5001"));
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            _messengerConsumerService.Received += _messengerConsumerService_Received;
-        }
-
-        private void _messengerConsumerService_Received(object? sender, Infrastructure.RabbitMqService.Events.MessageReceivedEventArgs<NewsLoadedEventArgs> e)
-        {
-            var newsResponse = _applicationNewsClient.GetNews(new NewsRequest { Id = e.Message.NewsId.ToString() });
-            
-            var news = new News(Guid.Parse(newsResponse.Id),
-                                newsResponse.SourceName,
-                                newsResponse.Title,
-                                newsResponse.Text,
-                                newsResponse.PublishDate.ToDateTime());
-            var namedEntityForms = _nerService.GetNamedEntityFormsFromNews(news);
-            
-            var exsistedNamedEntityForms = _namedEntityFormRepository.GetWhere(entity => namedEntityForms
-                                                                                             .Select(e => e.Value)
-                                                                                             .Contains(entity.Value));
-
-            var unexsistedNamedEntityForms = new List<NamedEntityForm>();
-            foreach (var namedEntityForm in namedEntityForms)
+            var exsistedNamedEntityForm = exsistedNamedEntityForms?.FirstOrDefault(e => e.Value == namedEntityForm.Value);
+            if (exsistedNamedEntityForm != null)
             {
-                var exsistedNamedEntityForm = exsistedNamedEntityForms?.FirstOrDefault(e => e.Value == namedEntityForm.Value);
-                if (exsistedNamedEntityForm != null)
-                {
-                    exsistedNamedEntityForm.AddNewsId(news.Id);
-                    _namedEntityFormRepository.Update(exsistedNamedEntityForm);
-                }
-                else
-                {
-                    unexsistedNamedEntityForms.Add(namedEntityForm);
-                }
+                exsistedNamedEntityForm.AddNewsId(news.Id);
+                _namedEntityFormRepository.Update(exsistedNamedEntityForm);
             }
-
-            foreach (var namedEntityForm in unexsistedNamedEntityForms)
+            else
             {
-                _namedEntityFormRepository.Add(namedEntityForm);
+                unexsistedNamedEntityForms.Add(namedEntityForm);
             }
-            //await _namedEntityFormAsyncRepository.AddRangeAsync(unexsistedNamedEntityForms);
-            _messengerConsumerService.AcknowledgeConsumeMessage(e.DeliveryTag);
         }
 
-        private async Task _messengerAsyncConsumerService_Received(object? sender, Infrastructure.RabbitMqService.Events.MessageReceivedEventArgs<NewsLoadedEventArgs> e)
+        foreach (var namedEntityForm in unexsistedNamedEntityForms)
         {
-            //var newsResponse = await _applicationNewsClient.GetNewsAsync(new NewsRequest { Id = e.Message.NewsId.ToString() });
-            //var news = new News(Guid.Parse(newsResponse.Id),
-            //                    newsResponse.SourceName,
-            //                    newsResponse.Title,
-            //                    newsResponse.Text,
-            //                    newsResponse.PublishDate.ToDateTime());
-            //var namedEntityForms = _nerService.GetNamedEntityFormsFromNews(news);
-            ////var exsistedNamedEntityForms = await _namedEntityFormAsyncRepository.GetWhereAsync(entity => namedEntityForms
-            ////                                                                                                 .Select(e => e.Value)
-            ////                                                                                                 .Contains(entity.Value));
-            //var exsistedNamedEntityForms = await _namedEntityFormAsyncRepository.GetByValueAsync(namedEntityForms.Select(e => e.Value));
-
-            //var unexsistedNamedEntityForms = new List<NamedEntityForm>();
-            //foreach (var namedEntityForm in namedEntityForms)
-            //{
-            //    var exsistedNamedEntityForm = exsistedNamedEntityForms?.FirstOrDefault(e => e.Value == namedEntityForm.Value);
-            //    if (exsistedNamedEntityForm != null)
-            //    {
-            //        exsistedNamedEntityForm.AddNewsId(news.Id);
-            //        await _namedEntityFormAsyncRepository.UpdateAsync(exsistedNamedEntityForm);
-            //    }
-            //    else 
-            //    {
-            //        unexsistedNamedEntityForms.Add(namedEntityForm);
-            //    }
-            //}
-
-            //foreach (var namedEntityForm in unexsistedNamedEntityForms) 
-            //{
-            //    await _namedEntityFormAsyncRepository.AddAsync(namedEntityForm);
-            //}
-            ////await _namedEntityFormAsyncRepository.AddRangeAsync(unexsistedNamedEntityForms);
-            //_messengerAsyncConsumerService.AcknowledgeConsumeMessage(e.DeliveryTag);
+            _namedEntityFormRepository.Add(namedEntityForm);
         }
+        //_namedEntityFormRepository.AddRange(unexsistedNamedEntityForms);           
+        _messengerConsumerService.AcknowledgeConsumeMessage(e.DeliveryTag);
     }
 }
